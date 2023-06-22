b0864a2cb80900250fa3d72e2d8695e7fc95d08f
sandiipgohel7 <gohelsandip025@gmail.com>
2023-06-22 13:03:53 +0530

    Add crypto app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import {  Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
+
 
 const Footer = () => {
   return (
@@ -25,10 +25,10 @@ const Footer = () => {
           </Text>
         </VStack>
 
-        <VStack>
+        {/* <VStack>
           <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
           <Text>Our Founder</Text>
-        </VStack>
+        </VStack> */}
       </Stack>
     </Box>
   );
